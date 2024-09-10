6080a5b6a73df60b3284fa1c99e79f65c353f1c0
LRD <LRD@LRD2>
2024-09-10 18:37:05 -0300

    Adicionar arquivos de projeto.

---
+++ MatrixCalculatorApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System.Windows.Forms;
+
+namespace MatrixCalculatorApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(102)))), ((int)(((byte)(244)))));
+            this.panelMenu.Controls.Add(this.buttonExit);
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(230, 734);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(102)))), ((int)(((byte)(244)))));
+            this.buttonExit.Font = new System.Drawing.Font("Bahnschrift Condensed", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonExit.Location = new System.Drawing.Point(21, 661);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(191, 52);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Sair";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 114);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(102)))), ((int)(((byte)(244)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(230, 114);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Calculadora de Matrizes";
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.White;
+            this.panelTitleBar.Controls.Add(this.richTextBox3);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(230, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(1032, 60);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.White;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox3.Font = new System.Drawing.Font("Bahnschrift Condensed", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox3.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(250, 60);
+            this.richTextBox3.TabIndex = 1;
+            this.richTextBox3.Text = "Calcular Determinante";
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(230, 60);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(1032, 674);
+            this.panelDesktop.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1262, 734);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Matrix Determinant Calculator";
+            this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panelTitleBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private Panel panelMenu;
+        private Panel panelTitleBar;
+        private Panel panelDesktop;
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button buttonExit;
+    }
+}
---
+++ MatrixCalculatorApp/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MatrixCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        private int[,] matrix;
+        private TableLayoutPanel tblMatrix;
+        private int matrixSize = 3; // Default matrix size
+        private Panel matrixPanel;
+        private ComboBox cmbMatrixSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Padding = new Padding(2);
+            this.BackColor = Color.FromArgb(98, 102, 244);
+
+            // Initialize Matrix Panel
+            InitializeMatrixPanel();
+        }
+
+        private void InitializeMatrixPanel()
+        {
+            // Create the panel where the matrix will be displayed
+            matrixPanel = new Panel
+            {
+                AutoScroll = true,
+                Dock = DockStyle.Fill,
+                BackColor = Color.WhiteSmoke
+            };
+
+            // Create ComboBox for selecting matrix size
+            cmbMatrixSize = new ComboBox
+            {
+                DropDownStyle = ComboBoxStyle.DropDownList,
+                Location = new Point(20, 80),
+                Size = new Size(100, 40)
+            };
+
+            // Populate ComboBox with matrix size options (from 2x2 to 5x5 for example)
+            for (int i = 1; i <= 10; i++) // You can adjust the range
+            {
+                cmbMatrixSize.Items.Add(i.ToString());
+            }
+            cmbMatrixSize.SelectedIndex = 1; // Default to 3x3
+
+            // Create buttons for matrix generation and determinant calculation
+            Button generateMatrixButton = new Button
+            {
+                Text = "Gerar Matriz",
+                Size = new Size(120, 40),
+                Location = new Point(20, 20)
+            };
+            generateMatrixButton.Click += GenerateMatrixButton_Click;
+
+            Button calculateDetButton = new Button
+            {
+                Text = "Calcular Determinante",
+                Size = new Size(160, 40),
+                Location = new Point(150, 20)
+            };
+            calculateDetButton.Click += CalculateDetButton_Click;
+
+            // Add controls to panelDesktop
+            panelDesktop.Controls.Add(generateMatrixButton);
+            panelDesktop.Controls.Add(calculateDetButton);
+            panelDesktop.Controls.Add(cmbMatrixSize);
+            panelDesktop.Controls.Add(matrixPanel);
+        }
+
+        // Generate the matrix grid dynamically
+        private void GenerateMatrixButton_Click(object sender, EventArgs e)
+        {
+            // Get matrix size from ComboBox
+            matrixSize = int.Parse(cmbMatrixSize.SelectedItem.ToString());
+
+            // Generate the matrix grid based on the selected size
+            GenerateMatrixGrid(matrixSize);
+        }
+
+        // Method to generate the matrix grid dynamically inside the matrixPanel
+        private void GenerateMatrixGrid(int size)
+        {
+            // Clear previous controls
+            matrixPanel.Controls.Clear();
+
+            // Create TableLayoutPanel to hold matrix cells
+            tblMatrix = new TableLayoutPanel
+            {
+                RowCount = size,
+                ColumnCount = size,
+                AutoSize = true,
+                Dock = DockStyle.None, // Remove DockStyle.Fill to allow manual positioning
+                AutoSizeMode = AutoSizeMode.GrowAndShrink
+            };
+
+            // Set the alignment to center
+            tblMatrix.Anchor = AnchorStyles.None;
+            tblMatrix.Margin = new Padding(10); // Optional: Adds space around the matrix
+
+            matrixPanel.Controls.Add(tblMatrix);
+
+            // Center the matrixPanel controls manually
+            tblMatrix.Location = new Point(
+                (matrixPanel.ClientSize.Width - tblMatrix.Width) / 2,
+                (matrixPanel.ClientSize.Height - tblMatrix.Height) / 2
+            );
+
+            matrix = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    TextBox txtBox = new TextBox
+                    {
+                        Name = $"txtMatrix_{i}_{j}",
+                        Width = 50,
+                        TextAlign = HorizontalAlignment.Center,
+                        BorderStyle = BorderStyle.FixedSingle
+                    };
+                    tblMatrix.Controls.Add(txtBox, j, i);
+                }
+            }
+
+            // Update location in case of resizing
+            tblMatrix.LocationChanged += (s, e) =>
+            {
+                tblMatrix.Location = new Point(
+                    (matrixPanel.ClientSize.Width - tblMatrix.Width) / 2,
+                    (matrixPanel.ClientSize.Height - tblMatrix.Height) / 2
+                );
+            };
+        }
+
+
+        // Calculate the determinant when the button is clicked
+        private void CalculateDetButton_Click(object sender, EventArgs e)
+        {
+            // Parse the values from TextBoxes into the matrix array
+            try
+            {
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        TextBox txtBox = (TextBox)tblMatrix.GetControlFromPosition(j, i);
+                        matrix[i, j] = int.Parse(txtBox.Text);
+                    }
+                }
+
+                int determinant = CalculateDeterminant(matrix);
+                MessageBox.Show($"O determinante é: {determinant}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao calcular o determinante: " + ex.Message);
+            }
+        }
+
+        // Function to calculate determinant recursively
+        private int CalculateDeterminant(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+            if (size == 1)
+                return matrix[0, 0];
+
+            if (size == 2)
+                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+
+            int determinant = 0;
+            for (int i = 0; i < size; i++)
+            {
+                int[,] subMatrix = CreateSubMatrix(matrix, 0, i);
+                determinant += (int)Math.Pow(-1, i) * matrix[0, i] * CalculateDeterminant(subMatrix);
+            }
+
+            return determinant;
+        }
+
+        // Function to create a submatrix for determinant calculation
+        private int[,] CreateSubMatrix(int[,] matrix, int excludeRow, int excludeCol)
+        {
+            int size = matrix.GetLength(0);
+            int[,] subMatrix = new int[size - 1, size - 1];
+            int r = -1;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i == excludeRow)
+                    continue;
+                r++;
+                int c = -1;
+                for (int j = 0; j < size; j++)
+                {
+                    if (j == excludeCol)
+                        continue;
+                    subMatrix[r, ++c] = matrix[i, j];
+                }
+            }
+
+            return subMatrix;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
